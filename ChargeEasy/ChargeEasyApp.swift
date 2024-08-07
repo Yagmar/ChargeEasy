06d7fd836819d40263df054aed013a2778661452
Yagmar <yagmardoumbia10@outlook.fr>
2024-08-07 20:57:57 +0200

    Initial Commit

---
+++ ChargeEasy/ChargeEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeEasyApp.swift
+//  ChargeEasy
+//
+//  Created by Abdoulaye Yagmar Doumbia on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChargeEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChargeEasyUITests/ChargeEasyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChargeEasyUITestsLaunchTests.swift
+//  ChargeEasyUITests
+//
+//  Created by Abdoulaye Yagmar Doumbia on 07/08/2024.
+//
+
+import XCTest
+
+final class ChargeEasyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
